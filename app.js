528a762bddd50961328d2193566fa0ef6997d69c
Aaron Sang <caramel.verve.0c@icloud.com>
2024-01-24 16:48:05 -0500

    Reworking for UI

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
+// Assign computer and player choices
 let getComputerChoice = () => {
   let options = ["zergling", "marine", "zealot"];
   return options[Math.floor(Math.random() * 3)];
 };
+let computerChoice = getComputerChoice();
 
-// zealot < marine < zergling
+let playerSelection;
+// function assignPlayer = () => {
+
+// }
+
+// match to DOM
+const zealot = document.querySelector("#zealot");
+const marine = document.querySelector("#marine");
+const zergling = document.querySelector("#zergling");
+let playerScore = document.querySelector("#player-score");
+let computerScore = document.querySelector("#computer-score");
 
 let playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
+  // zealot < marine < zergling
 
-  if (playerSelection === computerSelection) {
-    return `It's a tie!`;
-  } else if (playerSelection === "zergling" && computerSelection === "marine") {
-    return "You win! Zergling beats marine";
-  } else if (playerSelection === "marine" && computerSelection === "zealot") {
-    return `You win! Marine beats Zealot`;
-  } else if (playerSelection === "zealot" && computerSelection === "zergling") {
-    return "You win! Zealot beats Zergling";
+  if (playerSelection === computerChoice) {
+    console.log("You tied!");
+  } else if (playerSelection === "zergling" && computerChoice === "marine") {
+    console.log("You win! Zergling beats marine");
+  } else if (playerSelection === "marine" && computerChoice === "zealot") {
+    console.log(`You win! Marine beats Zealot`);
+  } else if (playerSelection === "zealot" && computerChoice === "zergling") {
+    console.log("You win! Zealot beats Zergling");
   } else {
-    return `You lose! ${playerSelection} lost to ${computerSelection}`;
+    console.log(`You lose! ${playerSelection} lost to ${computerChoice}`);
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose a unit: ");
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-
-    console.log(`Round ${i + 1}: ${result}`);
-  }
-};
-
-const results = game();
+zealot.addEventListener("click", playRound);
+marine.addEventListener("click", playRound);
+zergling.addEventListener("click", playRound);
