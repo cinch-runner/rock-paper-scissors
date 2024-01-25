764c6d1c00e637a1c314f0ff24681c47d3aa1778
Aaron Sang <caramel.verve.0c@icloud.com>
2024-01-25 10:51:31 -0500

    Logic finished

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
-// Assign computer and player choices
-let getComputerChoice = () => {
-  let options = ["zergling", "marine", "zealot"];
-  return options[Math.floor(Math.random() * 3)];
-};
-let computerChoice = getComputerChoice();
-
-let playerSelection;
-// function assignPlayer = () => {
-
-// }
-
 // match to DOM
 const zealot = document.querySelector("#zealot");
 const marine = document.querySelector("#marine");
 const zergling = document.querySelector("#zergling");
-let playerScore = document.querySelector("#player-score");
-let computerScore = document.querySelector("#computer-score");
+let playerScore = 0;
+let playerScoreElement = document.querySelector("#player-score");
+playerScoreElement.textContent = playerScore;
+let computerScore = 0;
+let computerScoreElement = document.querySelector("#computer-score");
+computerScoreElement.textContent = computerScore;
+let roundResultElement = document.querySelector("#round-result");
+let roundResultValue = "";
 
-let playRound = (playerSelection, computerSelection) => {
-  // zealot < marine < zergling
+let playerSelection;
+let computerChoice;
 
-  if (playerSelection === computerChoice) {
-    console.log("You tied!");
-  } else if (playerSelection === "zergling" && computerChoice === "marine") {
-    console.log("You win! Zergling beats marine");
-  } else if (playerSelection === "marine" && computerChoice === "zealot") {
-    console.log(`You win! Marine beats Zealot`);
-  } else if (playerSelection === "zealot" && computerChoice === "zergling") {
-    console.log("You win! Zealot beats Zergling");
-  } else {
-    console.log(`You lose! ${playerSelection} lost to ${computerChoice}`);
+// Assign computer choice
+let getComputerChoice = () => {
+  let options = ["zergling", "marine", "zealot"];
+  return options[Math.floor(Math.random() * 3)];
+};
+
+function assignPlayer(choice) {
+  playerSelection = choice;
+  computerChoice = getComputerChoice();
+  playRound(playerSelection, computerChoice);
+}
+
+let playRound = (playerSelection, getComputerChoice) => {
+  // zealot beats zergling, zergling beats marine, marine beats zealot
+  switch (playerSelection + computerChoice) {
+    case "zealotzergling":
+    case "marinezealot":
+    case "zerglingmarine":
+      roundResultElement.textContent = `You win! ${playerSelection} beats ${computerChoice}.`;
+      playerScore++;
+      playerScoreElement.textContent = playerScore;
+      break;
+    case "zealotmarine":
+    case "marinezergling":
+    case "zerglingzealot":
+      roundResultElement.textContent = `You lose! ${playerSelection} lost to ${computerChoice}.`;
+      computerScore++;
+      computerScoreElement.textContent = computerScore;
+      break;
+    case "marinemarine":
+    case "zealotzealot":
+    case "zerglingzergling":
+      roundResultElement.textContent = "It's a tie!";
+      break;
   }
 };
 
-zealot.addEventListener("click", playRound);
-marine.addEventListener("click", playRound);
-zergling.addEventListener("click", playRound);
+zealot.addEventListener("click", () => assignPlayer("zealot"));
+marine.addEventListener("click", () => assignPlayer("marine"));
+zergling.addEventListener("click", () => assignPlayer("zergling"));
